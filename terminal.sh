10298f5dabeddcf1fc902d31697c429358414d69
Mark Tse <mark.tse@neverendingqs.com>
2020-08-29 18:17:01 -0400

    feat: now runs command and returns std and exit code.

--- terminal.sh
+++ terminal.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo ${GH_EVENT_PAYLOAD}
+PREFIX="${CMD_PREFIX} "
+
+if ! [[ ${GH_COMMENT} == ${PREFIX}* ]]; then
+  # Only echo out 2x the prefix's length
+  echo "Comment '${GH_COMMENT:0:$(( ${#PREFIX}*2 + 1 ))}...' did not start with '${PREFIX}'. Skipping."
+  exit
+fi
+
+STDOUT=$(${GH_COMMENT:${#PREFIX}})
+
+echo "::set-output name=exit-code::$(echo $?)"
+echo "::set-output name=stdout::$(echo ${STDOUT})"
